6c5ab00204470e2a96cb8372fa469a6e4e446c6f
Oliver Phillip Roer <oliverroer@gmail.com>
2022-06-28 23:49:24 +0200

    use 3rd party dependency

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
-	fmt.Println("Hello, Wercker!")
+	data := [][]string{
+		{"Alfred", "15", "10/20", "(10.32, 56.21, 30.25)"},
+		{"Beelzebub", "30", "30/50", "(1,1,1)"},
+		{"Hortense", "21", "80/80", "(1,1,1)"},
+		{"Pokey", "8", "30/40", "(1,1,1)"},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"NPC", "Speed", "Power", "Location"})
+	table.AppendBulk(data)
+	table.Render()
 }
